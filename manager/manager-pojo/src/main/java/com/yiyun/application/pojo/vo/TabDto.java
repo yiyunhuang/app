3360417bf16e0b2b149fd3228db92d4993a7310e
翼云 <yiyunhuang1994@aliyun.com>
2020-01-03 00:13:49 +0800

    代码骨架搭建_web层

---
+++ manager/manager-pojo/src/main/java/com/yiyun/application/pojo/vo/TabDto.java
@@ -0,0 +1,13 @@
+package com.yiyun.application.pojo.vo;
+
+/**
+ * @author 翼云
+ * @version 1.0
+ * @ClassName TabDto
+ * @Description TODO  test
+ * @program: application
+ * @date 2020/1/2 22:08
+ **/
+
+public class TabDto {
+}
