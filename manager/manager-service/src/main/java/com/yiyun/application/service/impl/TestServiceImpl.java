5f9afd38fc0019e8418448d5e64f60a6c1107d3f
翼云 <yiyunhuang1994@aliyun.com>
2020-03-12 11:45:45 +0800

    service层添加注解

--- manager/manager-service/src/main/java/com/yiyun/application/service/impl/TestServiceImpl.java
+++ manager/manager-service/src/main/java/com/yiyun/application/service/impl/TestServiceImpl.java
@@ -7,6 +7,7 @@ import com.yiyun.application.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -19,6 +20,8 @@ import java.util.HashMap;
  * @program: application
  * @date 2020/1/13 0013 11:08
  **/
+
+@Service
 public class TestServiceImpl implements TestService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
--- manager/manager-web/src/main/java/com/yiyun/application/web/TestAction.java
+++ manager/manager-web/src/main/java/com/yiyun/application/web/TestAction.java
@@ -4,6 +4,8 @@ import com.yiyun.application.service.impl.TestServiceImpl;
 import com.yiyun.application.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,15 +21,18 @@ import java.util.HashMap;
  * @date 2020/1/13 0013 11:03
  **/
 @Controller
+@Scope("prototype")
 public class TestAction {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private TestService testService;
 
     @ResponseBody
     @RequestMapping("/test")
     public void test(){
         logger.info("TestAction/test");
-        TestService testService =new TestServiceImpl();
         HashMap map = new HashMap();
         map.put("test","insert");
         testService.test(map);
